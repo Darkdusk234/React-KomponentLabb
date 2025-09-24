9c8c7a3a37a17999570cbef096395bb416131d83
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-24 13:36:42 +0200

    Removed search input for director

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,6 @@ import React, { useState } from 'react'
 const SearchBar = (setFilmList) => {
     const [titleInput, setTitleInput] = useState("");
     const [yearInput, setYearInput] = useState("");
-    const [directorInput, setDirectorInput] = useState("");
 
     const Search = async () => {
         //Flytta Api nyckeln till en skyddad variabel t.ex. .env
@@ -37,10 +36,6 @@ const SearchBar = (setFilmList) => {
         <div className='SearchFieldBox'>
             <h4>Sök med släppnings år</h4>
             <input type="Sök med släppnings år" onChange={(e) => setYearInput=(e.target.value)}/>
-        </div>
-        <div className='SearchFieldBox'>
-            <h4>Sök med regissör</h4>
-            <input type="Sök med regissör" onChange={(e) => setDirectorInput=(e.target.value)}/>
         </div>
         <div className='SearchButton'>
             <button onClick={Search}>Sök</button>
